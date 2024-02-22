754705e971d57d4f1c826e32eda85d5c8f606645
Lbarberon <113266217+Lbarberon@users.noreply.github.com>
2024-02-22 20:38:47 -0300

    Add files via upload

--- TrabajoFinal/PC/main.c
+++ TrabajoFinal/PC/main.c
@@ -8,14 +8,18 @@
 
 char printSecuencia(void);
 void lecturaLocal(void);
+void config0(void);
+
+struct termios t_old, t_new, t_new1;
 
 char aux = 'E';
 
 int main(void) {
     int fd;
-    struct termios oldtty, newtty;
     char opcion;
-
+	
+	config0();
+	
     // Apertura del puerto serie
     fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
     if (fd == -1) {
@@ -24,33 +28,34 @@ int main(void) {
     }
 
     // Configuracion del TTY
-    if (termset(fd, 9600, &oldtty, &newtty) == -1) {
+    if (termset(fd, 9600, &t_old, &t_new) == -1) {
         printf("ERROR: No se pudo configurar el TTY.\n");
         return 1;
     }
 
     while(opcion != '9'){ // opcion 9 es volver al menu principal
+	  
       opcion = printSecuencia();
-
+	
       switch(opcion){
-          case '1': printf("Auto Fantastico (Enter para Salir)");
+          case '1': printf("Auto Fantastico (Enter para Salir)\n");
                     break;
-          case '2': printf("El Choque (Enter para Salir)");
+          case '2': printf("El Choque (Enter para Salir)\n");
                     break;
-          case '3': printf("La Apilada (Enter para Salir)");
+          case '3': printf("La Apilada (Enter para Salir)\n");
                     break;
-          case '4': printf("La Carrera (Enter para Salir)");
+          case '4': printf("La Carrera (Enter para Salir)\n");
                     break;
-          case '5': printf("Voy2Vuelvo1 (Enter para Salir)");
+          case '5': printf("Voy2Vuelvo1 (Enter para Salir)\n");
                     break;
-          case '6': printf("Juntos a la par (Enter para Salir)");
+          case '6': printf("Juntos a la par (Enter para Salir)\n");
                     break;
-          case '7': printf("Carga (Enter para Salir)");
+          case '7': printf("Carga (Enter para Salir)\n");
                     break;
-          case '8': printf("Brincos largos (Enter para Salir)");
+          case '8': printf("Brincos largos (Enter para Salir)\n");
                     break;
           case '9': break;
-          default:  printf("%d no es una opcion valida", opcion);
+          default:  printf("No es una opcion valida\n");
                     sleep(3);
                     break;
       }
@@ -60,6 +65,7 @@ int main(void) {
       write(fd, &opcion, 1);   // envia opcion a Rapberry
       tcdrain(fd);   // Espera hasta que se haya transmitido toda la salida escrita
 
+	  tcsetattr(FD_STDIN, TCSANOW, &t_new1);
       if((opcion >= 1) && (opcion < 9))
         while(aux != '\n'){ // enter finaliza la secuencia elegida
           aux = 'E'; // actualiza el valor de aux para un correcto funcionamiento de la funcion lecturaLocal
@@ -74,6 +80,7 @@ int main(void) {
         }
     }
     close(fd);
+	tcsetattr(FD_STDIN, TCSANOW, &t_old);
 
     return 0;
 }

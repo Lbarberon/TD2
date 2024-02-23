da50f089557c311d999ec00df4adb8c577209fb9
Lbarberon <113266217+Lbarberon@users.noreply.github.com>
2024-02-23 09:28:44 -0300

    Update main.c

--- TrabajoFinal/PC/main.c
+++ TrabajoFinal/PC/main.c
@@ -24,6 +24,8 @@ int main(void) {
         return -1;
     }
 
+    config0();
+    
     // Configuracion del TTY
     if (termset(fd, 9600, &t_old, &t_new) == -1) {
         printf("ERROR: No se pudo configurar el TTY.\n");
@@ -32,26 +34,26 @@ int main(void) {
 
     while(opcion != '9'){ // opcion 9 es volver al menu principal
       opcion = printSecuencia();
-
+      
       switch(opcion){
-          case '1': printf("Auto Fantastico (Enter para Salir)");
+          case '1': puts("Auto Fantastico (Enter para Salir)");
                     break;
-          case '2': printf("El Choque (Enter para Salir)");
+          case '2': puts("El Choque (Enter para Salir)");
                     break;
-          case '3': printf("La Apilada (Enter para Salir)");
+          case '3': puts("La Apilada (Enter para Salir)");
                     break;
-          case '4': printf("La Carrera (Enter para Salir)");
+          case '4': puts("La Carrera (Enter para Salir)");
                     break;
-          case '5': printf("Voy2Vuelvo1 (Enter para Salir)");
+          case '5': puts("Voy2Vuelvo1 (Enter para Salir)");
                     break;
-          case '6': printf("Juntos a la par (Enter para Salir)");
+          case '6': puts("Juntos a la par (Enter para Salir)");
                     break;
-          case '7': printf("Carga (Enter para Salir)");
+          case '7': puts("Carga (Enter para Salir)");
                     break;
-          case '8': printf("Brincos largos (Enter para Salir)");
+          case '8': puts("Brincos largos (Enter para Salir)");
                     break;
           case '9': break;
-          default:  printf("%d no es una opcion valida", opcion);
+          default:  puts("%d no es una opcion valida", opcion);
                     sleep(3);
                     break;
       }
@@ -60,7 +62,9 @@ int main(void) {
       tcflush(fd, TCIOFLUSH);  // Limpieza del bufer de entrada/salida
       write(fd, &opcion, 1);   // envia opcion a Rapberry
       tcdrain(fd);   // Espera hasta que se haya transmitido toda la salida escrita
-
+      
+      tcsetattr(FD_STDIN, TCSANOW, &t_new);
+      
       if((opcion >= 1) && (opcion < 9))
         while(aux != '\n'){ // enter finaliza la secuencia elegida
           aux = 'E'; // actualiza el valor de aux para un correcto funcionamiento de la funcion lecturaLocal
@@ -75,6 +79,7 @@ int main(void) {
         }
     }
     close(fd);
+    tcsetattr(FD_STDIN, TCSANOW, &t_old);
 
     return 0;
 }
